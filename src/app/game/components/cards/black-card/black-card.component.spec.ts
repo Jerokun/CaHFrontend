4653779b1fc41aa12d36c4352523c4050e66c514
[jerokun@gmail.com] <jerokun@gmail.com>
2021-04-17 01:19:11 +0200

    angular updated to v11

--- src/app/game/components/cards/black-card/black-card.component.spec.ts
+++ src/app/game/components/cards/black-card/black-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlackCardComponent } from './black-card.component';
 
@@ -6,7 +6,7 @@ describe('BlackCardComponent', () => {
   let component: BlackCardComponent;
   let fixture: ComponentFixture<BlackCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlackCardComponent ]
     })
--- src/app/game/components/cards/white-card/white-card.component.spec.ts
+++ src/app/game/components/cards/white-card/white-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WhiteCardComponent } from './white-card.component';
 
@@ -6,7 +6,7 @@ describe('WhiteCardComponent', () => {
   let component: WhiteCardComponent;
   let fixture: ComponentFixture<WhiteCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WhiteCardComponent ]
     })
